1e4350b1eaf2ef280b7f66c90ca5cab5858739a4
Berdiyor00 <berdiyorhasanov1@gmail.com>
2024-10-18 19:47:30 +0500

    Erp tizm

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+    return (
+      <nav className="fixed z-10 top-[100] left-0 md:top-0 md:left-0 right-0 md:right-auto bottom-0 md:bottom-auto md:w-[20%] w-full h-[60px] md:h-[100vh] bg-black">
+        <h1 className="text-white text-center p-4">Hello</h1>
+      </nav>
+    );
+  }
+  
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,19 @@
+import Navbar from "./Components/Navbar";
+
 export default function Home() {
-  return <div>Hello Berdiyor</div>;
+  return <><div className=" flex  flex-col-reverse  md:flex   ">
+
+    <Navbar/>
+ <div className="container ">
+
+   <div className="md:absolute  md:pl-[320px] md:left-0 md:right-0 bg-[red]">
+<p>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aut ad aliquid labore nisi et odio sunt aliquam quibusdam officiis possimus eius modi doloremque nesciunt incidunt, est quam, tempora odit.
+</p>
+
+  </div>
+ </div>
+  </div>
+
+  </>
 }
